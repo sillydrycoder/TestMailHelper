10f701c2c52ea5198fc5a1689bf8018e9a65aa77
Muhammad Ali <Muhammad_Ali@workmail.com>
2024-08-30 21:46:38 +0500

    Update setup.py to display Readme on PYPI

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='TestMailHelper',
-    version='0.1.0',
+    version='0.2.0',
     packages=find_packages(include=['testmailhelper', 'testmailhelper.*']),
     install_requires=[
         'requests',
     ],
     description='A Python package for interacting with the Testmail.app API.',
     long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',  # Add this line
     author='Muhammad Ali',
     author_email='muhammad_ali@workmail.com',
     url='https://github.com/tensor35/TestMailHelper',
