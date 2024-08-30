afa2f5d33ab361e15131cf2f08fb61b4d2273bb7
Muhammad Ali <muhammad_ali@workmail.com>
2024-08-30 18:43:13 +0500

    Rename package to TestMailHelper

--- setup.py
+++ setup.py
@@ -1,22 +1,19 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='tmclient',
+    name='TestMailHelper',
     version='0.1.0',
-    author='Your Name',
-    author_email='youremail@example.com',
-    description='A simple client for the testmail.app API',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/tmclient',
-    packages=find_packages(),
+    packages=find_packages(include=['testmailhelper', 'testmailhelper.*']),
     install_requires=[
         'requests',
     ],
+    description='A Python package for interacting with the Testmail.app API.',
+    author='Muhammad Ali',
+    author_email='muhammad_ali@workmail.com',
+    url='https://github.com/tensor35/TestMailHelper',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6',
 )
